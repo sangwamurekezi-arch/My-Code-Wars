8bf7058240f173f500c8b0bb4759074fbd5d76be
Yves Ntwali <sangwamurekezi@gmail.com>
2026-05-12 15:00:10 +0200

    Update solution - CodeHub

--- 7-kyu/lost-number-in-number-sequence/lost-number-in-number-sequence.js
+++ 7-kyu/lost-number-in-number-sequence/lost-number-in-number-sequence.js
@@ -1,21 +1,11 @@
-/*
 function findDeletedNumber(arr, mixArr) {
   // your code
   if (arr.length === mixArr.length){
     return 0;
   } else{
-    const 
+    let missingNum = arr.filter(ar => !mixArr.includes(ar));
+    const [x] = missingNum;
+    return x;
   }
 }
-*/
-function findDeletedNumber(arr, mixArr) {
-  let array1 = arr.reduce((acc,cur) => acc+cur, 0);
-  let array2 = mixArr.reduce((acc,cur) => acc+cur, 0);
-  let difference = array1 - array2;
-  if (difference === 0){
-    return 0;
-  } else{
-    return difference;
-  }
-}
-console.log(findDeletedNumber([1,2,3,4,5], [3,4,1,5]))
+​
