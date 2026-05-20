a5fbd5c83a032ef97719d6f60b5fb19331430c4c
Yves Ntwali <sangwamurekezi@gmail.com>
2026-05-20 14:38:54 +0200

    Add solution - CodeHub

---
+++ 8-kyu/reversed-sequence/reversed-sequence.js
@@ -0,0 +1,28 @@
+function reverseSeq(n){
+  let array = [];
+  for (let i = n; i >= 1; i-- ){
+    array.push(i);
+  }
+  return array;
+}
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+​
+/*const reverseSeq = n.map( numbrer=> {
+  let reversedin = array(n).split().reverse().join('');
+  return reversedin;
+});
+console.log(reverseSeq([7,6,5,4,2,1]));
+/*function reverseSeq(n){
+  let reversedHere = ;
+  for (i = n.length; i > 0 ; i--){}
+}*/
