34bff8ac8820b4d6e919fce3d3cb815a871c446a
Yves Ntwali <sangwamurekezi@gmail.com>
2026-07-22 18:44:51 +0200

    Update solution - CodeHub

--- 7-kyu/vowel-count/vowel-count.js
+++ 7-kyu/vowel-count/vowel-count.js
@@ -1,17 +1,12 @@
 function getCount(str) {
-  let simplifiedStr = str.toLowerCase();
-  let unified = simplifiedStr.match(/[aeiou]/gi) || [];
   let count = 0;
-  if (unified.length === 0){
-    return 0;
-  }
-  for(const uni of unified){
-    count++;
+  let vowels = ['a', 'e', 'i', 'o', 'u'];
+  
+  for (let char of str.toLowerCase().split('')) {
+    if (vowels.includes(char)) {
+      count++;
+    }
   }
   
   return count;
-  
 }
-​
-​
-​
