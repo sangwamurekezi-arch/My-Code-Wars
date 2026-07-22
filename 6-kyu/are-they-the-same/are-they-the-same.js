6b8bc57b2bbcc5f960bd84b980a6c969b8748c84
Yves Ntwali <sangwamurekezi@gmail.com>
2026-07-22 18:03:35 +0200

    Update solution - CodeHub

--- 6-kyu/are-they-the-same/are-they-the-same.js
+++ 6-kyu/are-they-the-same/are-they-the-same.js
@@ -1,43 +1,22 @@
-function comp(array1, array2){
-  if (array1 === null || array2 === null || array1 === undefined || array2 === undefined){
-    return false;
-  }
-  
-  if (array1.length !== array2.length){
-    return false;
-  }
-  
-  if (array1.length === 0 && array2.length === 0){
-    return true;
-  }
-  
-  // FIXED: Copy array2 to track multiplicities
-  let tempArray2 = [...array2];
-  let count = 0;
-  
-  for (let tik of array1){
-    const square = tik * tik;
-    const index = tempArray2.indexOf(square);
-    
-    if (index !== -1){
-      count++;
-      tempArray2.splice(index, 1);  // Remove the matched element
-    }
-  }
-  
-  return count === array1.length;
-}
-​
-​
-​
-​
-​
-// function comp(array1, array2){
-//   //your code here
-//   if (array1.length === null || array2 === null){
+//   }
+//   let count = 0;
+//   for (let tik of array1){
+// //     if (array2.include(tik)){
+// //       count+=1;
+// //     }
+//     let containing = array2.map(ar => {if (array2.includes(ar)) { count++; } else{count--}});
+//   }
+//   for (let tik of array1){
+//     const square = tik**2;
+//     let containing2 = array2.map(ar => {if (array2.includes(ar)) { count++; } else{count--}})
+//   }
+//   if (count > 0){
+//     return true;
+//   } else {
 //     return false;
 //   }
-//   if (array1.length !== array2.length){
-//     return false;
-//   }
-//   if (array1.length === 0 && array2.length === 0){
+// }
+console.log("Test 1:", comp([121, 144, 19, 161, 19, 144, 19, 11], 
+                            [121, 14641, 20736, 361, 25921, 361, 20736, 361]));
+console.log(comp([121, 144, 19, 161, 19, 144, 19, 11], [132, 14641, 20736, 361, 25921, 361, 20736, 361]))
+console.log(comp([121, 144, 19, 161, 19, 144, 19, 11], [231, 14641, 20736, 361, 25921, 361, 20736, 361]))
